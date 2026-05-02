869ef565712cd2fbf4584702739a61ee8979f114
Jordan Chang <113120687+Jojo-dwgait@users.noreply.github.com>
2026-05-02 10:31:44 -0700

    Time: 15 ms (0.83%), Space: 43.7 MB (9.78%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int addDigits(int num) {
+        String len = String.valueOf(num);
+        ArrayList<String> newNums = new ArrayList<String>();
+        for (int i = 0; i < len.length(); i++) {
+            System.out.println(len.substring(i, i+1));
+            newNums.add(len.substring(i, i+1));
+        }
+        int newNum = 0;
+        for (int j = 0; j < newNums.size(); j++) {
+            int backTo = Integer.parseInt(newNums.get(j));
+            newNum += backTo;
+        }
+        if (newNum > 9) {
+            return addDigits(newNum);
+        }
+        else {
+            return newNum;
+        }
+
+    }
+    
+}
